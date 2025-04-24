1cad6930b3fb2cc3e2a39c181afe66f5f056bb0c
aTechGuy27 <kumar.tech7358@gmail.com>
2025-04-25 01:17:19 +0530

    cross origin url change

--- src/main/java/com/byteandblog/controller/NewsController.java
+++ src/main/java/com/byteandblog/controller/NewsController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-@CrossOrigin(origins = "https://byteandblogapp.onrender.com")
+@CrossOrigin(origins = "https://byteandblog.onrender.com/")
 @RestController
 @RequestMapping("/api/news")
 public class NewsController {
